b4a67372a3b822c546af504d36d546b3041937af
Yoctoboy <16920060+Yoctoboy@users.noreply.github.com>
2023-05-12 18:05:23 +0200

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,13 @@ import { fetchTracks } from './lib/fetchTracks';
 import { AlbumCover } from './AlbumCover';
 
 const App = () => {
-  const trackUrls = [
-    'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
-    'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
-    'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
-    'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
-    'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
-  ];
   const [trackIndex, setTrackIndex] = useState(0);
 
   const goToNextTrack = () => {
     setTrackIndex(trackIndex + 1);
+  };
+  const goToPrevTrack = () => {
+    setTrackIndex(trackIndex - 1);
   };
   const { data: tracks } = useQuery({
     queryKey: ['tracks'],
@@ -33,10 +29,26 @@ const App = () => {
       <div className="App-images">
         {tracks && <AlbumCover track={tracks[0]} />}
       </div>
-      <div className="App-buttons"></div>
-      <audio src={tracks[trackIndex].url} autoPlay controls />
-      <button onClick={goToNextTrack}>Next track</button>
-      <p>{tracks && tracks[0].track.name}</p>
+      <div className="App-buttons">
+        {tracks && (
+          <>
+            <audio
+              src={tracks.at(trackIndex % 20).track.preview_url}
+              autoPlay
+              controls
+            />
+            <div style={{ display: 'flex' }}>
+              <button onClick={goToPrevTrack}>
+                {tracks.at((trackIndex - 1) % 20).track.name}
+              </button>
+              <button onClick={goToNextTrack}>
+                {tracks.at((trackIndex + 1) % 20).track.name}
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+      <p>{tracks && tracks[trackIndex].track.name}</p>
     </div>
   );
 };
